89fe5118ff379720d108ce1d79e6d4342f345fab
And4Web <anands2807@gmail.com>
2022-08-04 19:38:40 +0530

    material ui inline styling done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddIcon from '@mui/icons-material/Add';
+import { DeleteIcon, AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Material ui</h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained" color='secondary' size='small' startIcon={<SettingsIcon />}>Settings</Button>
+      <Button variant="outlined" startIcon={<AddIcon/>} sx={{backgroundColor: "red", color: "white", outline: "none", "&:hover":{background: "green", boxShadow: "5px 5px 10px grey"}}}>Add more</Button>
     </div>
   );
 }
